70fe267ae65a0b6cbd005663cc28f406090a5667
Robin Kristiansen <1232179+nibrobb@users.noreply.github.com>
2026-05-19 22:44:07 +0200

    Use bun instead

--- src/browser.ts
+++ src/browser.ts
@@ -1,2 +1,8 @@
+import * as QueryLogger from './index.js';
+
+if (typeof globalThis !== 'undefined') {
+  (globalThis as typeof globalThis & { QueryLogger?: typeof QueryLogger }).QueryLogger = QueryLogger;
+}
+
 export * from './index.js';
 
